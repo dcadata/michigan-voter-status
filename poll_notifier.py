258a4952cecaf23df516890d39576a73b3ff9f0a
Devon Ankar <devon.ankar@gmail.com>
2022-06-13 17:23:12 -0400

    Add poll notifier

---
+++ poll_notifier.py
@@ -0,0 +1,38 @@
+import re
+from time import sleep
+
+import requests
+from bs4 import BeautifulSoup
+
+from checker import send_email
+
+
+def _get_soup() -> BeautifulSoup:
+    response = requests.get('https://nitter.net/PollTrackerUSA/rss')
+    soup = BeautifulSoup(response.text, 'xml')
+    return soup
+
+
+def _get_relevant_tweets(soup: BeautifulSoup, include: list) -> list:
+    relevant_tweets = []
+    for tweet in soup.select('item'):
+        title, pubdate = map(lambda x: tweet.select_one(x).text.strip(), ('title', 'pubDate'))
+        if re.search('|'.join(include), title):
+            relevant_tweets.append(dict(title=title, pubdate=pubdate))
+    return relevant_tweets
+
+
+def _send_emails(email_bodies: list) -> None:
+    length = len(email_bodies)
+    for n, body in enumerate(email_bodies):
+        send_email(f'Poll Alert ({n + 1}/{length})', body)
+        sleep(1)
+
+
+def main() -> None:
+    relevant_tweets = _get_relevant_tweets(_get_soup(), ['#MI', '#MN'])
+    _send_emails(['{title} (PubDate: {pubdate})'.format(**tweet) for tweet in relevant_tweets])
+
+
+if __name__ == '__main__':
+    main()
