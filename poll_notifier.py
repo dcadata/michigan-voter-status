ddf55b091a43f2b853d824a77b96de2f23d4cece
Devon Ankar <devon.ankar@gmail.com>
2022-06-13 18:23:57 -0400

    Combine methods

--- poll_notifier.py
+++ poll_notifier.py
@@ -8,26 +8,25 @@ from bs4 import BeautifulSoup
 from voter_status_checker import send_email
 
 
-def _get_soup() -> BeautifulSoup:
+def _get_polls(keywords: list) -> list:
     response = requests.get('https://nitter.net/PollTrackerUSA/rss')
     soup = BeautifulSoup(response.text, 'xml')
-    return soup
 
-
-def _get_relevant_tweets(soup: BeautifulSoup, include: list) -> list:
     relevant_tweets = []
     tweets = soup.select('item')
     for tweet in tweets:
-        if tweet.find('link').text == open('data/poll_notifier_last_seen_link.txt').read():
+        if tweet.find('link').text == open('data/poll_notifier.txt').read():
             return []
         title, pubdate = map(lambda x: tweet.find(x).text.strip(), ('title', 'pubDate'))
-        if re.search('|'.join(include), title):
+        if re.search('|'.join(keywords), title):
             relevant_tweets.append(dict(title=title, pubdate=pubdate))
-    open('data/poll_notifier_last_seen_link.txt', 'w').write(tweets[0].find('link').text)
+
+    open('data/poll_notifier.txt', 'w').write(tweets[0].find('link').text)
+
     return relevant_tweets
 
 
-def _send_emails(email_bodies: list) -> None:
+def _send_polls_emails(email_bodies: list) -> None:
     length = len(email_bodies)
     for n, body in enumerate(email_bodies):
         send_email(f'Poll Alert ({n + 1}/{length})', body)
@@ -52,10 +51,10 @@ def _get_fte_gcb() -> str:
 
 
 def main() -> None:
+    if polls := _get_polls(['#MI']):
+        _send_polls_emails(['{title} (PubDate: {pubdate})'.format(**i) for i in polls])
     if fte_gcb := _get_fte_gcb():
         send_email('FTE GCB Alert', fte_gcb)
-    relevant_tweets = _get_relevant_tweets(_get_soup(), ['#MI'])
-    _send_emails(['{title} (PubDate: {pubdate})'.format(**tweet) for tweet in relevant_tweets])
 
 
 if __name__ == '__main__':
