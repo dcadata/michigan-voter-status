0779218f891f6ebe09042e17398f6f2117fd85b9
Devon Ankar <devon.ankar@gmail.com>
2022-06-13 18:11:48 -0400

    Add FTE GCB

--- poll_notifier.py
+++ poll_notifier.py
@@ -1,6 +1,7 @@
 import re
 from time import sleep
 
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
@@ -33,7 +34,26 @@ def _send_emails(email_bodies: list) -> None:
         sleep(1)
 
 
+def _get_fte_gcb() -> str:
+    data = pd.read_csv('https://projects.fivethirtyeight.com/polls/data/generic_ballot_averages.csv', usecols=[
+        'candidate', 'pct_estimate', 'election'])
+    data = data[data.election == '2022-11-08'].drop(columns=['election'])
+    data = data.iloc[-2:]
+    data.candidate = data.candidate.apply(lambda x: x[0])
+    data.pct_estimate = data.pct_estimate.apply(lambda x: round(x, 1))
+    estimates = data.groupby('candidate').pct_estimate.sum()
+    difference = 'R+{}'.format(round(estimates['R'] - estimates['D'], 1))
+    output = 'D: {D}\nR: {R}\n{difference}'.format(difference=difference, **estimates)
+
+    if output == open('fte_gcb.txt').read():
+        return ''
+    open('fte_gcb.txt', 'w').write(output)
+    return output
+
+
 def main() -> None:
+    if fte_gcb := _get_fte_gcb():
+        send_email('FTE GCB Alert', fte_gcb)
     relevant_tweets = _get_relevant_tweets(_get_soup(), ['#MI'])
     _send_emails(['{title} (PubDate: {pubdate})'.format(**tweet) for tweet in relevant_tweets])
 
