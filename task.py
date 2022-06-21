612053a76b7bb701e4ec76108ac63d0e65975ce5
Devon Ankar <devon.ankar@gmail.com>
2022-06-21 15:45:28 -0400

    Combine methods

--- task.py
+++ task.py
@@ -4,23 +4,20 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_voter_status(**params) -> None:
-    response = requests.post('https://mvic.sos.state.mi.us/Voter/SearchByName', data=params)
-    page = BeautifulSoup(response.text, 'lxml')
-    status = 'Registered: ' + ('Yes' if page.find(text='Yes, you are registered!') else 'No')
-    open('status.txt', 'w').write(status)
-
-
 def main() -> None:
     first_name, last_name = environ['NAME'].split(None, 1)
     birth_month, birth_year = environ['BIRTH_MONTH_AND_YEAR'].split('/', 1)
-    get_voter_status(
+    params = dict(
         FirstName=first_name,
         LastName=last_name,
         NameBirthMonth=birth_month,
         NameBirthYear=birth_year,
         ZipCode=environ['ZIP'],
     )
+    response = requests.post('https://mvic.sos.state.mi.us/Voter/SearchByName', data=params)
+    page = BeautifulSoup(response.text, 'lxml')
+    status = 'Registered: ' + ('Yes' if page.find(text='Yes, you are registered!') else 'No')
+    open('status.txt', 'w').write(status)
 
 
 if __name__ == '__main__':
